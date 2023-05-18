4ea7e6d4196c67a3e9d5d80bae34365382b3a0cd
Victoria Sales <contatovictoriasales@gmail.com>
2023-05-18 13:27:22 -0300

    Correção de erros lint

--- src/utils/readTalker.js
+++ src/utils/readTalker.js
@@ -6,7 +6,7 @@ const filePath = '../talker.json';
 async function readTalker() {
   try {
     const jsonData = await fs.readFile(join(__dirname, filePath), 'utf-8'); // leitura do arquivo
-    const data = JSON.parse(data); // análise do conteúdo do arquivo JSON em um objeto JavaScript
+    const data = JSON.parse(jsonData); // análise do conteúdo do arquivo JSON em um objeto JavaScript
     return data;
   } catch (err) {
     console.log(`Ocorreu o seguinte erro: ${err.message}`);
